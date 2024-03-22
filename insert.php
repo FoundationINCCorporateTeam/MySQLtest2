4e2c92f8dff62343c4bdd1cb75714fe4f7fb418f
FoundationINCCorporateTeam <130522379+FoundationINCCorporateTeam@users.noreply.github.com>
2024-03-22 10:09:02 -0500

    Create insert.php

---
+++ insert.php
@@ -0,0 +1,20 @@
+<?php
+$conn = mysqli_connect("sql300.infinityfree.com", "if0_36213692", "MN4444", "if0_36213692_mngames1");
+
+// Retrieve form data
+$search = $_POST['is'];
+
+
+// Construct SQL query
+$sql = "INSERT INTO searchdata (search_query) VALUES ('$search'')";
+
+// Execute the query
+if (mysqli_query($conn, $sql)) {
+    echo "Data inserted successfully!";
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
+
+// Close the database connection
+mysqli_close($conn);
+?>
